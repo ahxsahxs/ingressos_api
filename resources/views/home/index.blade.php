e92969393f3aaf0301ba9431250169a45aeec9dc
dev_antonio <antonio.henrique.if@gmail.com>
2018-04-13 22:21:06 -0300

    Index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,136 @@
 @extends('template.main')
 
+@section('title')
+Início
+@stop
+
 @section('content')
-	<h1>Começando o template da loja virtual</h1>
+<section class="section-slide pb-5">
+	<div class="owl-carousel">
+		<div class="item">
+			<img src="images/pictures/slide1.jpg" class="img-fluid" />
+		</div>
+	</div>
+</section>
+<section class="section-principal py-3">
+	<div class="container">
+		<div class="heading-title">
+			<h2>Eventos <span>destaques</span></h2>
+		</div>
+		<div class="eventos-lista py-3">
+			<div class="row">
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-3 col-12">
+					<a href="#" class="evento-item scroll-effect" style="background-image: url(images/pictures/evento.jpg)">
+						<div class="evento-data">10/02</div>
+						<h1>Camarote Galo</h1>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-newsletter" style="background-image: url('images/pictures/newsletter.jpg')">
+	<div class="container">
+		<form class="form-newsletter">
+			<div class="row w-75 mx-auto">
+				<div class="col-md-12 col-12 text-center">
+					<h2 class="titulo">Newsletter</h2>
+					<p class="text-white pb-2">Assine e fique por dentro do mundo da música!</p>
+				</div>
+				<div class="col-lg-10 col-md-9 col-12">
+					<input type="email" name="email" class="form-control form-newsletter-input" />
+				</div>
+				<div class="col-lg-2 col-md-3 col-12">
+					<button type="button" class="btn btn-success btn-rounded btn-assinar">Assinar</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</section>
+<footer class="footer-principal">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-3 col-12">
+				<h2>Formas de Pagamento</h2>
+				<img src="images/pictures/formas-pagamento.png" class="img-fluid" style="" />
+			</div>
+			<div class="col-md-3 col-sm-3 col-12">
+				<h2>Institucional</h2>
+				<ul>
+					<li><a href="#">Sobre nós</a></li>
+					<li><a href="#">Central de Ajuda</a></li>
+					<li><a href="#">Serviços</a></li>
+					<li><a href="#">Entre em Contato</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-3 col-12">
+				<h2>Políticas</h2>
+				<ul>
+					<li><a href="#">Políticas de Vendas</a></li>
+					<li><a href="#">Políticas de Privacidade</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-3 col-12">
+				<ul class="lista-horizontal mt-3">
+					<li>
+						<a href="#" class="rede-social-item"><i class="fa fa-facebook"></i></a>
+					</li>
+					<li>
+						<a href="#" class="rede-social-item"><i class="fa fa-instagram"></i></a>
+					</li>
+					<li>
+						<a href="tel:" class="rede-social-item"><i class="fa fa-phone"></i></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<footer class="footer-secundaria">
+	<div class="container">
+		© Desenvolvido por Agência Incrível
+	</div>
+</footer>
 @stop
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -4,11 +4,10 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>VIP Ingressos</title>
+	<title>VIP Ingressos - @yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="css/app.css">
-	<link rel="stylesheet" href="css/custom.css">
     <script src="js/app.js"></script>
     <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
     {{-- <script src="js/owl.carousel.min.js"></script> --}}
@@ -96,9 +95,73 @@
         </header>
     </main>
     
-    <main role="main" class="container" id='main-container'>
+    <main role="main" id='main-container'>
         @yield('content')
     </main>
+
+    <!-- Modal Entrar -->
+    <div class="modal fade" id="entrarModal" tabindex="-1" role="dialog" aria-labelledby="entrarModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="entrarModalLabel">Entrar</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="form-login font-xs">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <a href="#" class="btn btn-primary btn-facebook text-uppercase w-100 my-2">Conectar com o Facebook <i class="fa fa-facebook"></i></a>
+                                <p class="divider">ou</p>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label>E-mail</label>
+                                    <input type="email" name="email" class="form-control" />
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label>Senha</label>
+                                    <input type="password" name="senha" class="form-control" />
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <a href="#">Esqueci minha senha</a>
+                            </div>
+                            <div class="col-12 text-center">
+                                <button type="button" class="btn btn-success w-100 text-uppercase my-3 py-2">Entrar</button>
+                                <p class="divider mb-1">ou</p>
+                                <a href="#" class="text-dark font-weight-b">Faça seu cadastro</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Busca -->
+    <div class="modal fade modal-busca" id="buscaModal" tabindex="-1" role="dialog" aria-labelledby="buscaModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form class="form-busca-modal font-xs">
+                        <div class="row">
+                            <div class="col-9">
+                                <input type="text" name="query" placeholder="Buscar eventos, shows, teatros ..." class="form-control" />
+                            </div>
+                            <div class="col-3">
+                                <button type="button" class="btn-submit-busca"><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
